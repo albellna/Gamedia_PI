e35f7f0a30fe1d4915ec84a03080869df88cde50
CarlosDolz <cadogo@floridauniversitaria.es>
2021-02-12 12:19:33 +0100

    Añadiendo componentes

--- gamedia/src/app/app.module.ts
+++ gamedia/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CategoriasComponent } from './categorias/categorias.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CategoriasComponent,
+    QuienesSomosComponent,
+    ContactoComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
