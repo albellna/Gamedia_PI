8b603dd7b94d1f63275cf731906555609ed795ee
CarlosDolz <cadogo@floridauniversitaria.es>
2021-02-12 12:25:09 +0100

    Actualizando rutas

--- gamedia/src/app/app-routing.module.ts
+++ gamedia/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CategoriasComponent } from './categorias/categorias.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent}
-
+  {path: 'home', component: HomeComponent},
+  {path: 'categorias', component: CategoriasComponent},
+  {path: 'quienessomos', component: QuienesSomosComponent},
+  {path: 'contacto', component: ContactoComponent},
+  {path: 'login', component: LoginComponent},
 ];
 
 @NgModule({
