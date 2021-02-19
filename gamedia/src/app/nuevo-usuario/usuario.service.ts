7c0a40923235bc5c2bd0c512015f6962c1db9703
caromo <caromo@floridauniversitaria.es>
2021-02-19 10:18:38 +0100

    Añadiendo registro de usuarios

--- gamedia/src/app/nuevo-usuario/usuario.service.ts
+++ gamedia/src/app/nuevo-usuario/usuario.service.ts
@@ -1,23 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Usuario } from './model/usuario';
 import { NgModule } from '@angular/core';
 
-const cabecera = {headers: new HttpHeaders({'Content-TYpe': 'application/json'})};
+
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class UsuarioService {
   
   usuarioURL = "https://localhost:8000/api/users";
+  
 
   constructor(private httpClient: HttpClient) { }
 
-
+    getAll(): Observable<any> {
+      return this.httpClient.get<any>('/api/users')
+    }
+ 
   public crear(usuario: Usuario){
-    return this.httpClient.post<any>(this.usuarioURL ,usuario, cabecera);
+    return this.httpClient.post<any>(this.usuarioURL ,usuario);
   }
 
 }
