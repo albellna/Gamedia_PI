a1927ed6d6a4248eaf8eb89048a346098c78280b
Alvaro Bellmunt <albena@floridauniversitaria.es>
2021-02-22 11:14:25 +0100

    Gamedia PI - Cambiamos el token de servicio y creamos un archivo de servicios global.

--- gamedia/src/app/juego/juego.service.ts
+++ gamedia/src/app/juego/juego.service.ts
@@ -6,14 +6,12 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class JuegoService {
 
-  tokenURL = "https://id.twitch.tv/oauth2/token?client_id=1d3ja1yolgv4sy0cjqxtnnqy4qdfgx&client_secret=q5hm0x31bm2659rghs9z8cqmzljn15&grant_type=client_credentials"
   apiURL = "/v4/games"
   token = localStorage.getItem('access_token');
   cabecera: any;
   body: any;
   juegos = [] as any;
   
-
   constructor(private httpClient: HttpClient) { }
   
   public consultaJuego(token: string, id: string){
@@ -25,11 +23,5 @@ export class JuegoService {
       console.log("Error al comprobar el Token. No se ha proporcionado."); 
     }
     return this.httpClient.post(this.apiURL, this.body, this.cabecera);
-  }
-
-  public getToken(){
-    return this.httpClient.post(this.tokenURL, "").subscribe((resp: any) => {
-      localStorage.setItem('access_token', resp.access_token);
-    });
   }
 }
