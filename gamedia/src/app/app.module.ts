0336c259f95397b0b81f43d5025864f5522464a2
CarlosDolz <cadogo@floridauniversitaria.es>
2021-02-23 12:48:38 +0100

    Creando componente footer y quitando código repetido de footer

--- gamedia/src/app/app.module.ts
+++ gamedia/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NuevoUsuarioComponent } from './nuevo-usuario/nuevo-usuario.component';
 import { FormsModule } from '@angular/forms';
 import { BarraBusquedaComponent } from './barra-busqueda/barra-busqueda.component';
 import { ResultadoBusquedaComponent } from './resultado-busqueda/resultado-busqueda.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { ResultadoBusquedaComponent } from './resultado-busqueda/resultado-busqu
     JuegoComponent,
     NuevoUsuarioComponent,
     BarraBusquedaComponent,
-    ResultadoBusquedaComponent
+    ResultadoBusquedaComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
